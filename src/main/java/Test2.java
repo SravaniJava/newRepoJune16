4f26f968f76f9c43bd6b798b62c59dd901a8ee4d
sravaniPal <sravanigoudjava.com>
2026-06-16 18:25:35 +0530

    first commit done by master branch

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,5 +1,5 @@
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("this is what iam doing from sravani branch");
+        System.out.println("thisis  is what iam doing from sravanii branch");
     }
 }
